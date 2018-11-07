202ddf858c010e5ad15827224ca4954b0ef1efee
J M <G00329649@gmit.ie>
2018-11-07 16:58:15 +0000

    UI Timer added

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+    public GameObject timeIsUp, restartButton;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( TimeLeft.timeLeft <= 0 )
+        {
+            Time.timeScale = 0;
+            timeIsUp.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+        }
+	}
+
+    public void restartScene()
+    {
+        timeIsUp.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        TimeLeft.timeLeft = 10f;
+        SceneManager.LoadScene("Level1");
+
+    }
+}
