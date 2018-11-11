eb5dcec69a2b0ce12a03d02b27fdb177b575b3d5
J M <G00329649@gmit.ie>
2018-11-11 18:37:52 +0000

    Bullet completed

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public int health = 100;
+
+    public void TakeDamage(int damage)
+    {
+        //Debug.Log("damage taken");
+
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Death effects go here
+        
+        Destroy(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
