a28ad024ad528c0001044ec8b6a2aa40c964088c
J M <G00329649@gmit.ie>
2018-12-07 03:24:34 +0000

    Win level added and completed

---
+++ Assets/_Scripts/FinalScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinalScore : MonoBehaviour {
+    public Text text;
+
+    // Use this for initialization
+    void Start () { 
+        text = GetComponent<Text>();
+        text.text = "Final Score: " + ScoreScript.scoreValue;
+        StartCoroutine("WinSceneTimeout");
+    }
+
+    IEnumerator WinSceneTimeout()
+    {
+        float pauseTime = Time.realtimeSinceStartup + 20f;
+
+        while (pauseTime > Time.realtimeSinceStartup)
+        {
+            yield return 0;
+        }
+
+        //Debug.Log("Inside GameOverTimeout");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main");
+
+    }
+}
